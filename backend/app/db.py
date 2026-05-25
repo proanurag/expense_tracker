c5f8efed9346b0a2e9a66eb7151a731f6cf79de2
Anurag Tiwari <tiwarianurag280@gmail.com>
2026-05-25 20:25:26 +0530

    db changes

--- backend/app/db.py
+++ backend/app/db.py
@@ -8,6 +8,7 @@ class Base(DeclarativeBase):
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL =", DATABASE_URL)
 engine = create_async_engine(DATABASE_URL)
 database_session = async_sessionmaker(engine, expire_on_commit=False)
 
